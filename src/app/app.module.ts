3e2fa37a4354ef3d903a85f631970299f59efe00
Narayanan <Charan.Narayanan@ontario.ca>
2020-03-06 12:24:21 -0500

    new2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SafePipe } from './safe.pipe';
+import { SearchformComponent } from './searchform/searchform.component';
+import { textsearchComponent } from './textsearch/textsearch.component';
+import { MatTabsModule } from '@angular/material';
+import { MatTableModule } from '@angular/material';
+import { MatSortModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    SafePipe,    
+    SearchformComponent,
+    textsearchComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSortModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,      
+    BrowserAnimationsModule,
+    MatTableModule,
+    MDBBootstrapModule.forRoot(),
+    AngularFontAwesomeModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
